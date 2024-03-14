afe3fc7f68488ac6acf579c66102f2cdc5120d54
Najmus Sakib Dhrubo <najmussakibdhrubo52@gmail.com>
2024-03-14 23:39:06 +0600

    Created Employee model class and migration file

--- corporate_asset/models.py
+++ corporate_asset/models.py
@@ -1,8 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Company(models.Model):
     name = models.CharField(max_length=50)
-    contact_number = models.IntegerField(max_length=30)
+    contact_number = models.IntegerField()
     email = models.EmailField()
     address = models.TextField()
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=50)
+    company = models.ForeignKey('Company', related_name='employees', on_delete=models.CASCADE)
+    age = models.IntegerField()
+    address = models.TextField()
+    contact_number = models.IntegerField()
