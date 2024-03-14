def2d4c728ec6799c1ddeb8303b6c18ba842503c
Najmus Sakib Dhrubo <najmussakibdhrubo52@gmail.com>
2024-03-15 00:04:25 +0600

    Created Asset model and migrations

--- corporate_asset/models.py
+++ corporate_asset/models.py
@@ -15,3 +15,16 @@ class Employee(models.Model):
     age = models.IntegerField()
     address = models.TextField()
     contact_number = models.IntegerField()
+
+
+class Asset(models.Model):
+    company = models.ForeignKey('Company', related_name='assets', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=20, null=True)
+    price = models.IntegerField(null=True)               # Price of the asset
+    serial_number = models.CharField(max_length=50)      # Serial number of the asset
+    lent_to = models.ForeignKey('Employee', related_name='lent_devices', on_delete=models.SET_NULL, null=True)      # The employee the asset was lent to
+    condition_lent = models.TextField(null=True)         # Condition of the asset at the time of lending to the employee
+    condition_returned = models.TextField(null=True)     # Condition of the asset at the time of being returned by the employee
+    return_by = models.DateField(null=True)              # The date by which the employee has to return the asset
+    returned_at = models.DateField(null=True)            # The actual date the asset was returned
