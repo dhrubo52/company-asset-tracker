1352b2e638c2b33c6144d886d120c8d2cc3cc56d
Najmus Sakib Dhrubo <najmussakibdhrubo52@gmail.com>
2024-03-14 23:14:03 +0600

    Added company model class

--- corporate_asset/models.py
+++ corporate_asset/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    contact_number = models.IntegerField(max_length=30)
+    email = models.EmailField()
+    address = models.TextField()
